6751ae137a006166732bcda460f8cd9a011cd16c
Luca <lukatamro@gmail.com>
2025-04-15 09:39:08 +0200

    Ejercicios semana 1

---
+++ Curso1/Ejercicios/Semana1/App/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Ejercicio Nº 1 - Sintaxis básica
+        a) Escribir un programa en Java que muestre por pantalla un mensaje de Bienvenida a un usuario.
+        Por ejemplo: “Hola, bienvenido al sistema”.
+        b) Una vez logrado esta primera parte, permitir que se almacene el nombre del usuario en una variable
+        y se muestre el mismo en el mensaje. Por ejemplo: “Hola Eduardo, bienvenido al sistema”.
+        */
+        System.out.println("Hola, bienvenido al sistema");
+        String nombre = "Luca";
+        System.out.println("Hola " + nombre + ", bienvenido al sistema");
+    }
+}
